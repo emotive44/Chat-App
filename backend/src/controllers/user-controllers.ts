3b1f39af96790a7bdbe0d755c0c4bde10fa19685
emotive44 <marko_streleshki_96@abv.bg>
2020-12-14 21:36:58 +0200

    implement register user

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -12,7 +12,35 @@ const register = async (
 ): Promise<TResponse> => {
   const { name, email, password } = req.body as IUser;
 
-  console.log(name, email, password);
+  let existUser;
+  try {
+    existUser = await User.findOne({ email });
+  } catch (err) {
+    return next(new HttpError('Signing up failed, please try again.', 500));
+  }
+
+  if (existUser) {
+    return next(
+      new HttpError('User exist already, you can try with new data', 422)
+    );
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save();
+  } catch (err) {
+    return new HttpError('Register failed, please try again.', 500);
+  }
+
+  res.status(201).json({
+    userId: newUser.id,
+    name: newUser.name,
+  });
 };
 
 export { register };
--- backend/src/models/user-model.ts
+++ backend/src/models/user-model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
   name: string;
@@ -11,5 +12,20 @@ const userSchema: Schema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model<IUser>('User', userSchema);
